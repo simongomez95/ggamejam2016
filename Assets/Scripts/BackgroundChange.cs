098bd65c210b6bda774b3d993f45d129cbd61ade
unknown <simongomez95@gmail.com>
2016-01-30 12:16:40 -0500

    estovaafallar

---
+++ Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundChange : MonoBehaviour {
+
+    public Sprite spriteTrans;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Transformacion.estadoMundo==1)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = spriteTrans;
+        }
+	
+	}
+}
