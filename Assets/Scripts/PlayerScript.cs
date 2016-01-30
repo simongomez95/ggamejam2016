bf282cc80a6f02194f3dbf882109c69e5dcf477d
unknown <simongomez95@gmail.com>
2016-01-30 08:58:41 -0500

    Cambiado nombre de nuevo

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	bool salto = true;
+	public float speed;
+	public GameObject bullet;
+	float movingSpeed = 0;
+	private Rigidbody2D rb2D;  
+	//Animator anim;
+	bool allowFire = true;
+
+
+
+	void Start()
+	{
+		//anim = GetComponent<Animator> ();
+	}
+
+	IEnumerator Fire(){
+		allowFire = false;
+		Instantiate(bullet, transform.position, transform.rotation);
+		yield return new WaitForSeconds (0.5f);
+		allowFire = true;
+	}
+
+	void Update()
+	{
+		
+
+		//if (Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0) {
+		//	anim.SetBool ("isMoving", true);			
+		//} else {
+		//	anim.SetBool("isMoving", false);
+		//}
+
+
+	}
+
+	void OnTriggerEnter2D() {
+	}
+
+	void FixedUpdate()
+	{
+
+		//limitar la rotacion a ejes XY
+		//transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		//GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+		//Movimiento en el mapa
+		float input_y = Input.GetAxis("Vertical");
+		float input_x = Input.GetAxis("Horizontal");
+		if (Input.GetKeyDown ("space")&& salto == true){
+			salto = false;
+			rb2D = GetComponent<Rigidbody2D> ();
+			rb2D.AddForce (Vector2.up * 10, ForceMode2D.Impulse);
+		} 
+		if (transform.position.y < (1)) {
+			salto = true;
+		}
+
+		transform.position = new Vector3(transform.position.x + input_x * speed, transform.position.y, transform.position.z);
+
+	}
+}
