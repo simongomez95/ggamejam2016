05f1926a2ddfbc23a6585f1d24439711c4d7a643
unknown <simongomez95@gmail.com>
2016-01-31 12:02:36 -0500

    kinematic magic

--- Assets/Scripts/Transformacion.cs
+++ Assets/Scripts/Transformacion.cs
@@ -36,17 +36,46 @@ public class Transformacion : MonoBehaviour {
             }
             else
             {
+                
                 estadoMundo = 0;
             }
         }
         if (estadoMundo == 1)
         {
+            PararTiempo(0);
             tiempoTrans = tiempoTrans - Time.deltaTime;
         }
         else
         {
+            PararTiempo(1);
             tiempoTrans = 10;
             allowTransform = true;
+        }
+    }
+
+    void PararTiempo(int funcion)
+    {
+        GameObject[] objetos = GameObject.FindGameObjectsWithTag("Environment");
+        if (funcion == 0)
+        {
+            foreach (GameObject objeto in objetos)
+            {
+                if (objeto.GetComponent<Rigidbody2D>() != null)
+                {
+                    objeto.GetComponent<Rigidbody2D>().isKinematic = true;
+                }
+            }
+        }
+        else if (funcion==1)
+        {
+            //Despara el tiempo?
+            foreach (GameObject objeto in objetos)
+            {
+                if (objeto.GetComponent<Rigidbody2D>() != null)
+                {
+                    objeto.GetComponent<Rigidbody2D>().isKinematic = false;
+                }
+            }
         }
     }
 
